0f9e9a80e300eee6c7cb95219b205f3099361d6f
David L Valentine <davidlewisvalentine@gmail.com>
2024-12-01 13:48:37 -0800

    finish day 1, got both stars!

--- Day 1/solution.js
+++ Day 1/solution.js
@@ -33,3 +33,42 @@ util.processProblemInput('./answer_input.txt', (data) => {
     // Output
     console.log(`Total distance: ${distanceTotal}`)
 });
+
+// Part 2 (succeeded)
+util.processProblemInput('./answer_input.txt', (data) => {
+    const leftCol = [];
+    const rightCol = [];
+
+    // Read data, split list up and convert text to numbers
+    data.split('\n').forEach((row, idx) => {
+        const cols = row.split(' ');
+        if (cols.length) {
+            leftCol[idx] = parseInt(cols[0], 10) || 0;
+            rightCol[idx] = parseInt(cols[cols.length - 1].replace('\r', ''), 10) || 0;
+        }
+    });
+
+    // Sort the lists so that we can pair smallest numbers
+    leftCol.sort();
+    rightCol.sort();
+
+    // Input
+    console.log('Input: ');
+    console.table(leftCol);
+    console.table(rightCol);
+
+    // Iterate over left list, count the times it appears in the right list, mult left list item by that and add to simScore
+    let simScore = 0;
+    
+    leftCol.forEach((num, idx) => {
+        let timesAppear = 0;
+        rightCol.forEach((pair) => {
+            timesAppear += pair === num ? 1 : 0;
+        });
+
+        simScore += Math.abs(num * timesAppear);
+    });
+
+    // Output
+    console.log(`Total distance: ${simScore}`)
+});
