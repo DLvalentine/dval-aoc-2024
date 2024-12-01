12083076e9e8812aab6545dc3a0d9e439b479ece
David L Valentine <davidlewisvalentine@gmail.com>
2024-12-01 13:52:02 -0800

    cleanup

--- Day 1/solution.js
+++ Day 1/solution.js
@@ -1,74 +1,81 @@
 import * as util from '../util.js';
 
 // Part 1 (succeeded)
-util.processProblemInput('./answer_input.txt', (data) => {
-    const leftCol = [];
-    const rightCol = [];
-
-    // Read data, split list up and convert text to numbers
-    data.split('\n').forEach((row, idx) => {
-        const cols = row.split(' ');
-        if (cols.length) {
-            leftCol[idx] = parseInt(cols[0], 10) || 0;
-            rightCol[idx] = parseInt(cols[cols.length - 1].replace('\r', ''), 10) || 0;
-        }
-    });
-
-    // Sort the lists so that we can pair smallest numbers
-    leftCol.sort();
-    rightCol.sort();
-
-    // Input
-    console.log('Input: ');
-    console.table(leftCol);
-    console.table(rightCol);
-
-    // Iterate over left list, pair with an item on the right list, and subtract the difference to store in total distance
-    let distanceTotal = 0;
+const partOne = () => {
+    util.processProblemInput('./answer_input.txt', (data) => {
+        const leftCol = [];
+        const rightCol = [];
     
-    leftCol.forEach((num, idx) => {
-        distanceTotal += Math.abs((num - rightCol[idx])); 
+        // Read data, split list up and convert text to numbers
+        data.split('\n').forEach((row, idx) => {
+            const cols = row.split(' ');
+            if (cols.length) {
+                leftCol[idx] = parseInt(cols[0], 10) || 0;
+                rightCol[idx] = parseInt(cols[cols.length - 1].replace('\r', ''), 10) || 0;
+            }
+        });
+    
+        // Sort the lists so that we can pair smallest numbers
+        leftCol.sort();
+        rightCol.sort();
+    
+        // Input
+        console.log('Input: ');
+        console.table(leftCol);
+        console.table(rightCol);
+    
+        // Iterate over left list, pair with an item on the right list, and subtract the difference to store in total distance
+        let distanceTotal = 0;
+        
+        leftCol.forEach((num, idx) => {
+            distanceTotal += Math.abs((num - rightCol[idx])); 
+        });
+    
+        // Output
+        console.log(`Total distance: ${distanceTotal}`)
     });
-
-    // Output
-    console.log(`Total distance: ${distanceTotal}`)
-});
+};
 
 // Part 2 (succeeded)
-util.processProblemInput('./answer_input.txt', (data) => {
-    const leftCol = [];
-    const rightCol = [];
-
-    // Read data, split list up and convert text to numbers
-    data.split('\n').forEach((row, idx) => {
-        const cols = row.split(' ');
-        if (cols.length) {
-            leftCol[idx] = parseInt(cols[0], 10) || 0;
-            rightCol[idx] = parseInt(cols[cols.length - 1].replace('\r', ''), 10) || 0;
-        }
-    });
-
-    // Sort the lists so that we can pair smallest numbers
-    leftCol.sort();
-    rightCol.sort();
-
-    // Input
-    console.log('Input: ');
-    console.table(leftCol);
-    console.table(rightCol);
-
-    // Iterate over left list, count the times it appears in the right list, mult left list item by that and add to simScore
-    let simScore = 0;
+const partTwo = () => {
+    util.processProblemInput('./answer_input.txt', (data) => {
+        const leftCol = [];
+        const rightCol = [];
     
-    leftCol.forEach((num, idx) => {
-        let timesAppear = 0;
-        rightCol.forEach((pair) => {
-            timesAppear += pair === num ? 1 : 0;
+        // Read data, split list up and convert text to numbers
+        data.split('\n').forEach((row, idx) => {
+            const cols = row.split(' ');
+            if (cols.length) {
+                leftCol[idx] = parseInt(cols[0], 10) || 0;
+                rightCol[idx] = parseInt(cols[cols.length - 1].replace('\r', ''), 10) || 0;
+            }
         });
-
-        simScore += Math.abs(num * timesAppear);
+    
+        // Sort the lists so that we can pair smallest numbers
+        leftCol.sort();
+        rightCol.sort();
+    
+        // Input
+        console.log('Input: ');
+        console.table(leftCol);
+        console.table(rightCol);
+    
+        // Iterate over left list, count the times it appears in the right list, mult left list item by that and add to simScore
+        let simScore = 0;
+        
+        leftCol.forEach((num, idx) => {
+            let timesAppear = 0;
+            rightCol.forEach((pair) => {
+                timesAppear += pair === num ? 1 : 0;
+            });
+    
+            simScore += Math.abs(num * timesAppear);
+        });
+    
+        // Output
+        console.log(`Total distance: ${simScore}`)
     });
+};
 
-    // Output
-    console.log(`Total distance: ${simScore}`)
-});
+partOne();
+partTwo();
--- util.js
+++ util.js
@@ -1,6 +1,6 @@
 import * as fs from 'node:fs';
 
-
+// File reading boilerplate - provide path to input file and a callback to process the data
 const processProblemInput = (path, callback) => {
     try {
         const data = fs.readFileSync(path, 'utf8');
